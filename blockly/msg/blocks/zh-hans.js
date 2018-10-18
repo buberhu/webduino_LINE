bf20f32249d14b16e63d09ed6ecf21fda3c05880
buberhu <44234763+buberhu@users.noreply.github.com>
2018-10-18 23:11:51 +0800

    Update zh-hans.js

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,4 +1,5 @@
 Blockly.Msg.LINE_MSG = '讯息 :';
-Blockly.Msg.LINE_STK = '表情贴图 STKPKGID :';
+Blockly.Msg.LINE_STKPKG = '表情贴图 STKPKGID :';
+Blockly.Msg.LINE_STK = '表情贴图 STKID :';
 Blockly.Msg.LINE_IMGTHUM = '预览图网址 :';
 Blockly.Msg.LINE_IMGFULL = '图片网址 :';
