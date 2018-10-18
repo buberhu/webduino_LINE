1a46633a79696b573aecc3ea850d348023cf2f0b
buberhu <44234763+buberhu@users.noreply.github.com>
2018-10-18 23:11:13 +0800

    Update zh-hant.js

--- blockly/msg/blocks/zh-hant.js
+++ blockly/msg/blocks/zh-hant.js
@@ -1,4 +1,5 @@
 Blockly.Msg.LINE_MSG = '訊息 :';
-Blockly.Msg.LINE_STK = '表情貼圖 STKPKGID :';
+Blockly.Msg.LINE_STKPKG = '表情貼圖 STKPKGID :';
+Blockly.Msg.LINE_STK = '表情貼圖 STKID :';
 Blockly.Msg.LINE_IMGTHUM = '預覽圖網址 :';
 Blockly.Msg.LINE_IMGFULL = '圖片網址 :';
