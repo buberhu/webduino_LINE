75319f3b0b1f73ecaec7752a0551600ac700e027
buberhu <44234763+buberhu@users.noreply.github.com>
2018-10-18 23:42:50 +0800

    Update en.js

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -1,5 +1,5 @@
+Blockly.Msg.LINE_TOKEN = 'Token :';
 Blockly.Msg.LINE_MSG = 'Message :';
 Blockly.Msg.LINE_STKPKG = 'Sticker STKPKGID :';
 Blockly.Msg.LINE_STK = 'Sticker STKID :';
-Blockly.Msg.LINE_IMGTHUM = 'Image Thumbnail URL :';
-Blockly.Msg.LINE_IMGFULL = 'Image FullSize URL :';
+Blockly.Msg.LINE_IMGFULL = 'Image URL :';
