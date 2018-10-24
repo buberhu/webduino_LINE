e7103c6f19d25768f7a2899f108ad1fb14371d3f
buber hu <44234763+buberhu@users.noreply.github.com>
2018-10-24 15:34:18 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -4,7 +4,7 @@ Blockly.Blocks['line_ifttt'] = {
       .setCheck(null)
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField("LINE ( IFTTT )")
-      .appendField("Event :");
+      .appendField("Event Name :");
     this.appendValueInput("line_ifttt_key")
       .setCheck(null)
       .setAlign(Blockly.ALIGN_RIGHT)
@@ -48,7 +48,7 @@ Blockly.Blocks['line_notify'] = {
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.LINE_STK);
-   this.appendValueInput("line_notify_imagefullsize")
+    this.appendValueInput("line_notify_imagefullsize")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.LINE_IMGFULL);
@@ -65,16 +65,16 @@ Blockly.Blocks['linebot_set'] = {
   init: function() {
     this.appendDummyInput()
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Set LINE Bot")
+        .appendField(Blockly.Msg.LINEBot_Setbot)
         .appendField(new Blockly.FieldVariable("linebot"), "name_");
     this.appendValueInput("linebot_token")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("token :");
+        .appendField(Blockly.Msg.LINEBot_Token);
     this.appendValueInput("linebot_userid")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("user ID :");
+        .appendField(Blockly.Msg.LINEBot_Userid);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(195);
@@ -87,10 +87,10 @@ Blockly.Blocks['linebot_on'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Start Receiving Message");
+        .appendField(Blockly.Msg.LINEBot_Onrece);
     this.appendStatementInput("on_")
         .setCheck(null)
-        .appendField("Do");
+        .appendField(Blockly.Msg.LINEBot_Ondo);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -103,7 +103,7 @@ Blockly.Blocks['linebot_message'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Received Message");
+        .appendField(Blockly.Msg.LINEBot_Recemsg);
     this.setOutput(true, null);
     this.setColour(30);
  this.setTooltip("");
@@ -115,9 +115,9 @@ Blockly.Blocks['linebot_sendmsg'] = {
   init: function() {
     this.appendValueInput("bot_sendmsg")
         .setCheck("String")
-        .appendField("Set")
+        .appendField(Blockly.Msg.LINEBot_Set)
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Send Message :");
+        .appendField(Blockly.Msg.LINEBot_Sendmsg);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -129,9 +129,9 @@ Blockly.Blocks['linebot_sendmsg'] = {
 Blockly.Blocks['linebot_sendstk'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Set")
+        .appendField(Blockly.Msg.LINEBot_Set)
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Send Sticker，");
+        .appendField(Blockly.Msg.LINEBot_Sendstk);
     this.appendValueInput("bot_sendstkpkg")
         .setCheck("Number")
         .appendField("STKPKGID :");
@@ -150,17 +150,17 @@ Blockly.Blocks['linebot_sendstk'] = {
 Blockly.Blocks['linebot_sendimg'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Set")
+        .appendField(Blockly.Msg.LINEBot_Set)
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Send Image");
+        .appendField(Blockly.Msg.LINEBot_Sendimg);
     this.appendValueInput("bot_sendimg_s")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Preview URL :");
+        .appendField(Blockly.Msg.LINEBot_SendimgP);
     this.appendValueInput("bot_sendimg")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Original URL :");
+        .appendField(Blockly.Msg.LINEBot_SendimgO);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -172,17 +172,17 @@ Blockly.Blocks['linebot_sendimg'] = {
 Blockly.Blocks['linebot_sendvideo'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Set")
+        .appendField(Blockly.Msg.LINEBot_Set)
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Send Video");
+        .appendField(Blockly.Msg.LINEBot_Sendvideo);
     this.appendValueInput("bot_sendvideo_s")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Image URL :");
+        .appendField(Blockly.Msg.LINEBot_SendvideoP);
     this.appendValueInput("bot_sendvideo")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Video URL :");
+        .appendField(Blockly.Msg.LINEBot_SendvideoO);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -194,17 +194,17 @@ Blockly.Blocks['linebot_sendvideo'] = {
 Blockly.Blocks['linebot_sendaudio'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Set")
+        .appendField(Blockly.Msg.LINEBot_Set)
         .appendField(new Blockly.FieldVariable("linebot"), "name_")
-        .appendField("Send Audio");
+        .appendField(Blockly.Msg.LINEBot_Sendaudio);
     this.appendValueInput("bot_sendaudio_s")
         .setCheck("Number")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("duration :");
+        .appendField(Blockly.Msg.LINEBot_SendaudioP);
     this.appendValueInput("bot_sendaudio")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Audio URL :");
+        .appendField(Blockly.Msg.LINEBot_SendaudioO);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
